dd228cf055a36367a7d5efb85b48f5cf76aec4c4
lucasbclarke <lucasbaileyclarke@outlook.com>
2025-12-17 15:12:18 +1100

    added option menu when mouse is clicked, renamed file

--- testGui.java
+++ testGui.java
@@ -1,31 +1,26 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
 import static javax.swing.JOptionPane.*;
 
-public class Main {
+public class testGui {
     
     public static void main(String ar[]) {
-        Frame f = new Frame();
+        Frame f = new Frame("Custom Checkbox application");
 
         Label l1 = new Label("Select option");
 
         l1.setBounds(150, 50, 120, 80);
         f.add(l1);
 
-        // CheckBox created
         Checkbox c2 = new Checkbox("1");
         c2.setBounds(150, 150, 80, 50);
         f.add(c2);
 
-        // CheckBox created
         Checkbox c3 = new Checkbox("2");
         c3.setBounds(150, 200, 80, 50);
         f.add(c3);
 
-        // CheckBox created
         Checkbox c4 = new Checkbox("3");
         c4.setBounds(150, 250, 80, 50);
         f.add(c4);
@@ -43,6 +38,26 @@ public class Main {
             }
         });
 
+        PopupMenu pm = new PopupMenu("Select an option");
+        pm.add("Option 1");
+        pm.add("Option 2");
+        pm.add("Option 3");
+        pm.add("Option 4");
+        pm.add("Option 5");
+        f.add(pm); 
+
+        f.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                if (e.isPopupTrigger()) {
+                }
+            }
+            public void mouseReleased(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON3) {
+                    pm.show(f, e.getX(), e.getY());
+                }
+           }
+
+        });
 
     }
 }
