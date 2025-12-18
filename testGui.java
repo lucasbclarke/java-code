e865fffb2a1787bccadd60bf346ea0c84007a326
lucasbclarke <lucasbaileyclarke@outlook.com>
2025-12-18 15:07:43 +1100

    added new event listener for each checkbox to display whether it is selected or not, also added an event listener for all checkboxes to display which checkboxes are selected when there are multiple, also seperated logic into a new function

--- testGui.java
+++ testGui.java
@@ -1,42 +1,91 @@
 import java.awt.*;
 import java.awt.event.*;
 
+import javax.swing.Action;
+
 import static javax.swing.JOptionPane.*;
 
-public class testGui {
-    
-    public static void main(String ar[]) {
+public class Gui {
+    Checkbox c2, c3, c4;
+    Label l1;
+
+    public void TestGui() {
         Frame f = new Frame("Custom Checkbox application");
 
-        Label l1 = new Label("Select option");
+        l1 = new Label("Select option");
 
-        l1.setBounds(150, 50, 120, 80);
+        l1.setBounds(150, 50, 280, 80);
         f.add(l1);
 
-        Checkbox c2 = new Checkbox("1");
+        c2 = new Checkbox("1");
         c2.setBounds(150, 150, 80, 50);
         f.add(c2);
 
-        Checkbox c3 = new Checkbox("2");
+        c2.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    l1.setText("You have selected the first checkbox");
+                } else {
+                    l1.setText("Seclect Option");
+                }
+            }
+        });
+
+        c3 = new Checkbox("2");
         c3.setBounds(150, 200, 80, 50);
         f.add(c3);
 
-        Checkbox c4 = new Checkbox("3");
+        c3.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    l1.setText("You have selected the second checkbox");
+                } else {
+                    l1.setText("Seclect Option");
+                }
+            }
+        });
+
+        c4 = new Checkbox("3");
         c4.setBounds(150, 250, 80, 50);
         f.add(c4);
 
-        f.setSize(500, 500);
-        f.setLayout(null);
-        f.setVisible(true);
-
-        f.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                if (showConfirmDialog(f, "Are you sure you want to exit?", "Exiting Application!", YES_NO_OPTION, WARNING_MESSAGE) == YES_OPTION) {
-                    f.dispose();
+        c4.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    l1.setText("You have selected the third checkbox");
+                } else {
+                    l1.setText("Seclect Option");
                 }
-
             }
         });
+
+        ItemListener sharedListener = new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                if (c2.getState() && c3.getState() && c4.getState()) {
+                    l1.setText("All checkboxes are selected");
+                } else if (c2.getState() && c3.getState() && !c4.getState()) {
+                    l1.setText("You have selected the first and second checkbox");
+                } else if (c2.getState() && !c3.getState() && c4.getState()){
+                    l1.setText("You have selected the first and third checkbox");
+                } else if (!c2.getState() && c3.getState() && c4.getState()){
+                    l1.setText("You have selected the second and third checkbox");
+                } else if (c2.getState() && !c3.getState()) {
+                    l1.setText("You have selected the first checkbox");
+                } else if (!c2.getState() && c3.getState()) {
+                    l1.setText("You have selected the second checkbox");
+                } else if (c4.getState() && !c3.getState()) {
+                    l1.setText("You have selected the third checkbox");
+                } else if (!c4.getState() && c3.getState()) {
+                    l1.setText("You have selected the second checkbox");
+                }
+
+
+            }
+        };
+        
+        c2.addItemListener(sharedListener);
+        c3.addItemListener(sharedListener);
+        c4.addItemListener(sharedListener);
 
         PopupMenu pm = new PopupMenu("Select an option");
         pm.add("Option 1");
@@ -55,9 +104,37 @@ public class testGui {
                 if (e.getButton() == MouseEvent.BUTTON3) {
                     pm.show(f, e.getX(), e.getY());
                 }
-           }
+            }
 
         });
 
+        Button b1 = new Button("OK");
+        b1.setBounds(150, 300, 80, 50);
+        f.add(b1);
+
+        b1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                l1.setText("You have clicked the button");
+            }
+        });
+
+        f.setSize(500, 500);
+        f.setLayout(null);
+        f.setVisible(true);
+
+        f.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                if (showConfirmDialog(f, "Are you sure you want to exit?", "Exiting Application!", YES_NO_OPTION, WARNING_MESSAGE) == YES_OPTION) {
+                    System.exit(0);
+                }
+
+            }
+        });
     }
+
+    public static void main(String ar[]) {
+        Gui g = new Gui();
+        g.TestGui();
+    }
+
 }
